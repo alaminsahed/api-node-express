992bb76aa7b7bf9ef1e26fbf311dea6ca58021f9
alaminsahed <alaminsahed97@gmail.com>
2021-09-26 08:51:31 +0600

    mongo start

--- my-mongo/index.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/students')
-.then(()=>console.log("mongodb connected"))
-.catch((err)=>console.error("database failed"));
--- routers/studentRouter.js
+++ routers/studentRouter.js
@@ -1,75 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const data = require('../data');
 
 
 const studentList =(req,res)=>{
 
-    //when use promise resolve, access data by then
-    data.getStudent()
-    .then(data=>{
-        res.send(data);
-    })
+   
   
 }
 
 
 const studentInformation = (req,res)=>{
     const id = parseInt (req.params.id);
-    data.getStudent()
-    .then(students=>{
-        const student = students.find(s=>s.id === id);
-        student? res.send(student) : res.status(404).send("Not found by this id");
-    })
+  
 }
 
 
 const addStudent = (req, res)=>{
  
         const student = req.body;
-        data.getStudent()
-        .then(students=>{
-            
-            students.push(student)
-            // all students rewrite here.
-            data.insertStudent(students)
-            .then(newStudent =>{
-                //newStudent is the response of resolve
-                res.send(students)
-            })
-        })
-       
+    
     }
 
 const editStudent = (req,res)=>{
     const id = parseInt(req.params.id)
     const updateStudent = req.body;
-    data.getStudent()
-    .then(students=>{
-      const student = students.find(s=>s.id === id);
-      if(!student) res.status(404).send("Not found with this id")
-      else{
-          const i = students.findIndex(s=> s.id ===id);
-          students[i] = updateStudent;
-          data.insertStudent(students)
-          .then(msg=> res.send(updateStudent));
-      }
-    })
+  
 
 }
 
 const deleteStudent = (req, res) =>{
     const id = parseInt(req.params.id);
-    data.getStudent()
-    .then(students=>{
-        const student = students.find(s=>s.id === id);
-        if(!student) res.status(404).send("not found by this is")
-        else{
-            const allStudents = students.filter(s => s.id !== id);
-            data.insertStudent(allStudents)
-            .then(msg=>res.send(students));
-        }
-    })
+   
 }
 
 
